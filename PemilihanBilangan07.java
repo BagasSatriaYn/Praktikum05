3e84f7cf157d907edb689cbb94f22292794992ab
BagasSatriaYn <duhjvdedhvhjufahfy@gmail.com>
2023-10-04 14:35:25 +0700

    Percobaan 3

--- PemilihanBilangan07.java
+++ PemilihanBilangan07.java
@@ -2,18 +2,14 @@ import java.util.Scanner;
 
 public class PemilihanBilangan07{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner input07 = new Scanner(System.in);
 
         System.out.print("Masukkan sebuah angka: ");
-        int angka = sc.nextInt();
+        int angka = input07.nextInt();
 
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        String hasil;
+        hasil = (angka > 0.0 ) ? "Bilangan Positif" : "Bilangan Negatif";
+        System.out.println(angka+ " adalah " + hasil );
+         }
     }
-}
+
